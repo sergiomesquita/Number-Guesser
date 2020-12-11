f5810b44328bc2473bfc5560ce1f29dbd90440ac
Sergio Mesquita <sergiomesquita@Air-de-Sergio.home>
2020-12-11 01:39:24 +0000

    final commit

--- script.js
+++ script.js
@@ -3,4 +3,34 @@ let computerScore = 0;
 let currentRoundNumber = 1;
 
 // Write your code below:
+function generateTarget(number) {
+     return Math.floor(Math.random() * 10); 
+};
 
+function compareGuesses (currentHumanGuess, computerGuess, target) {
+    if(Math.abs(currentHumanGuess - target) < Math.abs(computerGuess - target)) {
+        return true;
+    }
+
+    else if(Math.abs(computerGuess - target) < Math.abs(currentHumanGuess - target)) {
+        return false;
+    }
+
+    else {
+        return false;
+    }
+};
+
+function updateScore (winner) {
+    if(winner === "human") {
+        humanScore ++;
+    }
+
+    else {
+        computerScore ++ ;
+    }
+};
+
+function advanceRound() {
+    currentRoundNumber ++;
+}
